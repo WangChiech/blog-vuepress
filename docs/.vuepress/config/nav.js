53181272730da4f9e299779d6b6c1b9b7beca596
WangChiech <wangchiech@yeah.net>
2021-12-16 16:50:23 +0800

    调整主题相关

--- docs/.vuepress/config/nav.js
+++ docs/.vuepress/config/nav.js
@@ -1,24 +1,31 @@
 // nav
 module.exports = [
+  // {
+  //   text: '语言',
+  //   link: '/language/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+  //   items: [
+  //     // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
+  //     {
+  //       text: 'JavaScript',
+  //       link: '/pages/8d5317/',
+  //       // items: [
+  //       //   { text: '/pages/8d5317/', link: '/pages/8d5317/' },
+  //       // ],
+  //     }
+  //   ],
+  // },
   {
-    text: '语言',
-    link: '/language/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+    text: '工具',
+    link: '/pages/f74aeb/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
     items: [
       // 说明：以下所有link的值只是在相应md文件定义的永久链接（不是什么特殊生成的编码）。另外，注意结尾是有斜杠的
       {
-        text: 'JavaScript',
-        link: '/pages/8d5317/',
+        text: 'Echarts',
+        link: '/pages/f74aeb/',
         // items: [
         //   { text: '/pages/8d5317/', link: '/pages/8d5317/' },
         // ],
-      },
-      // {
-      //   text: 'Node',
-      //   // link: '/pages/8d5317/',
-      //   items: [
-      //     // { text: '《JavaScript教程》', link: '/pages/8d5317/' },
-      //   ],
-      // },
+      }
     ],
   },
   // {
